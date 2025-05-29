a58f98e52282c71c5130ce0d5ae6e7b449a0b371
nususu1004 <nususu1004@gmail.com>
2025-05-29 13:18:13 +0900

    20250529 html 기초

--- day01/script.js
+++ day01/script.js
@@ -20,6 +20,24 @@ if ($screen && $toggleButton) {
   //   });
   $toggleButton.addEventListener("click", () => {
     stopWatchOn = !stopWatchOn;
-    console.log("stopWathOn", stopWatchOn);
+    $toggleButton.innerHTML = stopWatchOn ? "■" : "▶";
+    // innerHTML은 실제로 HTML태그값도 먹히는 반면 innerText는 텍스트값 그대로 나온다.
+    $toggleButton.classList.toggle("stop-color");
+    // $toggleButton.style.backgroundColor = stopWatchOn ? "tomato" : "steelblue";
+    if (stopWatchOn) {
+      // 첫번째 인자(아규먼트): 콜백함수
+      // 두번째 인자(아규먼트): 숫자(Number) - ms
+      timeInterval = setInterval(() => {
+        seconds++;
+        const mm = String(Math.floor(seconds / 6000) % 60).padStart(2, "0");
+        const ss = String(Math.floor(seconds / 100) % 60).padStart(2, "0");
+        const cs = String(seconds % 100).padStart(2, "0");
+        $screen.innerText = `${mm}:${ss}:${cs}`;
+      }, 10);
+    } else {
+      clearInterval(timeInterval);
+      seconds = 0;
+      $screen.innerText = "00:00:00";
+    }
   });
 }
